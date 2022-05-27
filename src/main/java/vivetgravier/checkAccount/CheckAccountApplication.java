a2837271132656d34a29dc8a267a12ed6fd8c833
leograv5 <gravierleo@live.fr>
2022-05-27 16:34:54 +0200

    init

---
+++ src/main/java/vivetgravier/checkAccount/CheckAccountApplication.java
@@ -0,0 +1,13 @@
+package vivetgravier.checkAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vivetgravier/checkAccount/CheckAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package vivetgravier.checkAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
